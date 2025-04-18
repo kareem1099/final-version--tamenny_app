e1ee6c204aab64d9699438af282f65e80cf88fd6
Kareem <117325791+kareem1099@users.noreply.github.com>
2025-04-18 05:15:23 +0200

    Update completed_screen.dart

--- lib/scan/presentation/views/completed_screen.dart
+++ lib/scan/presentation/views/completed_screen.dart
@@ -1,4 +1,4 @@
-طimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
